0a1962d43d225a1c7b52fd3bb1b156ffc2aafb1e
Yusuf Faraji <yusuf.faraji@gmail.com>
2021-06-08 22:49:54 +0100

    Simplify serverless export line

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,21 @@ import serverlessExpress from '@vendia/serverless-express'
 
 dotEnvConfig();
 
-let server: any;
+// let server: any;
 
-if (process.env.LOCAL === "true") {
-  const PORT = 3232;
-  app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-    console.log(`Access via http://localhost:${PORT}`);
-  });
-} else {
-  server = serverlessExpress( {app} );
-}
+// if (process.env.LOCAL === "true") {
+//   const PORT = 3232;
+//   app.listen(PORT, () => {
+//     console.log(`Listening on port ${PORT}`);
+//     console.log(`Access via http://localhost:${PORT}`);
+//   });
+// } else {
+//   server = serverlessExpress( {app} );
+// }
 
-export const handler = (event: any, context: any) => {
-  console.log("Handler", event, context);
-  server;
-};
+// export const handler = (event: any, context: any) => {
+//   console.log("Handler", event, context);
+//   server;
+// };
+
+exports.handler = serverlessExpress( {app} )
